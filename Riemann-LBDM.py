c064b4a47f7dbc6550051090a2ade5a745ec3157
drproduck <duckhiem95@yahoo.com>
2019-04-28 04:20:25 -0700

    created Riemann-LBDM.py'

--- Riemann-LBDM.py
+++ Riemann-LBDM.py
@@ -12,40 +12,114 @@ from myutils import *
 from stochastic_solvers import *
 from util.metrics import get_accuracyk
 
-class Solver(StochasticOptimizer):
-    '''No retraction. Initialized with a valid solution.
+class RiemannianSolver: '''Stochastic Riemannian Eigensolver
+        
+        Args:
+          batchsize: number of data sampled for each udpate
+          npass: aka epoch, number of passes through the complete data
+
+          stepsize_type:
+          stepsize_init: 
+          stepsize_lambda:
+
     '''
     def __init__(self, A, k, **opt):
-        StochasticOptimizer.__init__(self, **opt)
+        self.batchsize = setOption(100, opt.get('batchsize'), lambda x: x > 0)
+        self.npass = setOption(10, opt.get('npass'), lambda x: x > 0)
+        self.stepsize_type = setOption('decay', opt.get('stepsize_type'), lambda x: type(x) is str and x in ['decay', 'fixed', 'hybrid', 'KHA/et'])
+        self.stepsize_init = setOption(5, opt.get('stepsize_init'), lambda x: x >= 0)
+        self.stepsize_lambda = setOption(1e-3, opt.get('stepsize_lambda'), lambda x: x >= 0)
+        
+        self.inneriter = setOption(20, opt.get('inneriter'), lambda x: x > 0)
+        self.checkperiod = setOption(self.inneriter, opt.get('checkperiod'), lambda x: type(x) is int and x > 0)
+        
+        self.vars = {} # This 'catch-all' dict is reserved for situations when inheriting class needs to get variable that isn't observed.
+
         self.A = A
         self.n, self.m = A.shape
         self.k = k
         self.inneriter = math.ceil(self.n / self.batchsize)
         self.checkperiod = setOption(self.inneriter, opt.get('checkperiod'), lambda x: type(x) is int and x > 0)
         
+    def train(self, X=None, recorder=None):
+        self.initialize()
         
+        if X is None:
+            X = self.init_X()
+        
+        X_update_operator = self.get_update_operator(X)
+        info = {'loss_hist': [], 'time': []}
+        
+        if recorder is None:
+            recorder = {}
+            
+        for key in recorder:
+            if key not in info:
+                info[key] = []
+            
+        print('Iter                 Loss            Time')
+    
+        for epoch in range(1,self.npass+1):
+            indexer = self.getIndexer()
+
+            for t in range(self.inneriter):
+                start = time()
+                global_t = (epoch - 1)*self.inneriter + t
+
+                batch = indexer(t)
+                
+                G = self.getGradient(X, batch)
+                
+                X = X_update_operator(X, -G)
+
+
+                if global_t > 0 and global_t % self.checkperiod == 0: 
+                    time_elapsed = time() - start
+                    info['loss_hist'].append(self.getCost(X))
+                    info['time'].append(global_t)
+                    
+                    for stat, func in recorder.items():
+                        info[stat].append(recorder[stat](X))
+
+                    print('{0:4d}         {1:10e}        {2:5f}'.format(info['time'][-1], info['loss_hist'][-1], time_elapsed))
+                    
+                self.vars = {} # clear all hidden variables here
+    
+        return X, info
+        
+    
+    def get_update_operator(self, X):
+        
+        if self.stepsize_type == 'KHA/et':
+            X_update_operator = optimize.KHAet(X)
+        
+        else:
+            X_update_operator = optimize.Vanilla(stepsize_init=self.stepsize_init, stepsize_type=self.stepsize_type, stepsize_lambda=self.stepsize_lambda)
+        return X_update_operator
+
+
     def init_X(self): 
         X = np.random.randn(self.m, self.k)
         X, _ = np.linalg.qr(X, 'reduced')
         return X
 
     
-    def getIndexer(self):
+    def get_indexer(self):
         perm = np.random.permutation(self.n)
         
-        def getBatch(t):
+        def get_batch(t):
             lowr = t*self.batchsize
             uppr = np.minimum((t+1)*self.batchsize, self.n)
             return perm[lowr: uppr]
             
-        return getBatch    
+        return get_batch    
     
             
-    def getCost(self, X):
+    def get_cost(self, X):
         return self.vars['batch_cost']
     
     
-    def getGradient(self, X, batch):
+    def get_gradient(self, X, batch):
         Abatch = self.A[batch,:]
         batchsize = Abatch.shape[0]
 
@@ -70,11 +144,10 @@ class Solver(StochasticOptimizer):
         return super().train(recorder=recorder)
 
 
-def RiemannLBDM(A, k, n_samples, solver_func):
+def rlbdm(A, k, n_samples, solver_func):
     n = A.shape[0]
     colperm = np.random.choice(n, l, replace=False)
     C = A[:, colperm]
-    print('column sum: ', C.sum(axis=0))
     
     X, info = solver_func(C, k)
     
